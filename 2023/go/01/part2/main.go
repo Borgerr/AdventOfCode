6b92087a8a09c4e0d6ba3a185dc235746be9a052
ash <97464181+Borgerr@users.noreply.github.com>
2023-12-01 10:02:58 -0700

    basic idea for part 2

--- 2023/go/01/part2/main.go
+++ 2023/go/01/part2/main.go
@@ -6,26 +6,92 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strconv"
-	"unicode"
+	"strings"
 )
 
-func processLine(line string, matcher *Regexp) int {
-	digits := make([]rune, 0, 50)
-	for _, c := range line {
-		if unicode.IsDigit(c) {
-			digits = append(digits, c)
+func getMinWithIndex(array []int) (int, int) {
+	min := array[0]
+	minIndex := 0
+	for i := 1; i < len(array); i++ {
+		if min > array[i] {
+			min = array[i]
+			minIndex = i
+		}
+	}
+	return min, minIndex
+}
+
+func getMaxWithIndex(array []int) (int, int) {
+	max := array[0]
+	maxIndex := 0
+	for i := 1; i < len(array); i++ {
+		if max < array[i] {
+			max = array[i]
+			maxIndex = i
+		}
+	}
+	return max, maxIndex
+}
+
+func processLine(line string) int {
+	returned := 0
+	// find earliest digit first
+	digitIndexes := [9]int{0}
+	digits := [9]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	wordIndexes := [9]int{0}
+	words := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for index, word := range words {
+		earliest := strings.Index(line, word)
+		if earliest != -1 {
+			wordIndexes[index] = earliest
 		}
 	}
 
-	firstAndLast := []rune{digits[0], digits[len(digits)-1]}
-
-	i, err := strconv.Atoi(string(firstAndLast))
-	if err != nil {
-		panic(err)
+	for index, digit := range digits {
+		earliest := strings.Index(line, digit)
+		if earliest != -1 {
+			digitIndexes[index] = earliest
+		}
 	}
-	return i
+
+	earliestDigit, earliestDigIndex := getMinWithIndex(digitIndexes[:])
+	earliestWord, earliestWordIndex := getMinWithIndex(wordIndexes[:])
+
+	if earliestDigit < earliestWord {
+		returned += (earliestDigIndex + 1) * 10
+	} else {
+		returned += (earliestWordIndex + 1) * 10
+	}
+
+	// do the same and find latest digit
+	digitIndexes = [9]int{0}
+	wordIndexes = [9]int{0}
+
+	for index, word := range words {
+		latest := strings.LastIndex(line, word)
+		if latest != -1 {
+			wordIndexes[index] = latest
+		}
+	}
+
+	for index, digit := range digits {
+		latest := strings.LastIndex(line, digit)
+		if latest != -1 {
+			wordIndexes[index] = latest
+		}
+	}
+
+	latestDigit, latestDigIndex := getMaxWithIndex(digitIndexes[:])
+	latestWord, latestWordIndex := getMaxWithIndex(wordIndexes[:])
+
+	if latestDigit > latestWord {
+		returned += latestDigIndex + 1
+	} else {
+		returned += latestWordIndex + 1
+	}
+
+	return returned
 }
 
 func main() {
@@ -45,12 +111,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// get regexp
-	matcher := regexp.MustCompile(`one|two|three|four|five|six|seven|eight|nine`)
-
 	total := 0
 	for _, line := range lines {
-		fmt.Printf("%d\n", processLine(line, matcher))
+		fmt.Printf("%d\n", processLine(line))
 		total += processLine(line)
 	}
 
