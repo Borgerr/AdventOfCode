8b5d472a4d38c1116818a9d4f129e3e528a0d1fe
ash <97464181+Borgerr@users.noreply.github.com>
2023-11-30 22:45:50 -0700

    I/O stuff, line processing + input for 01-1

--- 2023/go/01/part1/main.go
+++ 2023/go/01/part1/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"unicode"
 )
 
+func processLine(line string) int {
+	digits := make([]rune, 2, 2)
+	for _, c := range line {
+		if unicode.IsDigit(c) {
+			digits = append(digits, c)
+		}
+	}
+
+	i, err := strconv.Atoi(string(digits))
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	lines := make([]string, 1000, 1000)
+	absPath, _ := filepath.Abs("./01/part1/input.txt")
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	total := 0
+	for _, line := range lines {
+		total += processLine(line)
+	}
+
+	fmt.Printf("%d\n", total)
 }
