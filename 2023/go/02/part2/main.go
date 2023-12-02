cdab9e87a161957d7d5547d8c76f44fef0c1ebde
ash <97464181+Borgerr@users.noreply.github.com>
2023-12-02 10:12:48 -0700

    day 2 part 2

---
+++ 2023/go/02/part2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func gamePower(gameSet [][]int) int {
+	maxRed := 0
+	maxGreen := 0
+	maxBlue := 0
+	for _, set := range gameSet {
+		if set[0] > maxRed {
+			maxRed = set[0]
+		}
+		if set[1] > maxGreen {
+			maxGreen = set[1]
+		}
+		if set[2] > maxBlue {
+			maxBlue = set[2]
+		}
+	}
+
+	return maxRed * maxGreen * maxBlue
+}
+
+func main() {
+	lines := make([][][]int, 0, 100)
+	absPath, _ := filepath.Abs("./02/part2/input.txt")
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	redRe := regexp.MustCompile("[0-9]+ red")
+	greenRe := regexp.MustCompile("[0-9]+ green")
+	blueRe := regexp.MustCompile("[0-9]+ blue")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fullLine := strings.Split(scanner.Text(), ": ")[1]
+		gameSet := make([][]int, 0, 20)
+		for _, stringSet := range strings.Split(fullLine, "; ") {
+			red := 0
+			green := 0
+			blue := 0
+			redPart := redRe.FindString(stringSet)
+			if redPart != "" {
+				i, err := strconv.Atoi(strings.Split(redPart, " red")[0])
+				if err != nil {
+					panic(err)
+				}
+				red = i
+			}
+			greenPart := greenRe.FindString(stringSet)
+			if greenPart != "" {
+				i, err := strconv.Atoi(strings.Split(greenPart, " green")[0])
+				if err != nil {
+					panic(err)
+				}
+				green = i
+			}
+			bluePart := blueRe.FindString(stringSet)
+			if bluePart != "" {
+				i, err := strconv.Atoi(strings.Split(bluePart, " blue")[0])
+				if err != nil {
+					panic(err)
+				}
+				blue = i
+			}
+			gameSet = append(gameSet, []int{red, green, blue})
+		}
+		lines = append(lines, gameSet)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	total := 0
+	for _, line := range lines {
+		total += gamePower(line)
+	}
+
+	fmt.Println(total)
+}
